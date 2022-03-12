78d502606974c1b689b119b32fe21b2181817a42
Václav Doležal <bc.va.do@gmail.com>
2022-03-12 06:01:50 +0100

    deleted comments in JS

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-// alert("Ahojky")
-
-//alert("Ahojky")
 
 
 
-//strings     "$" => right alt + 36, "~" => alt + 1, "`" => right ALt + 96,
 
-//let number1 = 10;
-//let numebr2 = 20;
-
-//console.log(`There are two numbers: ${number1} nad ${numebr2} :-).`)
 
 
 
