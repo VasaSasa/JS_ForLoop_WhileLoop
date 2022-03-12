03d169cc00a7af7375e1e758fe324725a3975a5a
Václav Doležal <bc.va.do@gmail.com>
2022-03-12 07:32:29 +0100

    dealed with task one in JS, not finished yet

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 
+// „ XYZ“  Alt+0132 -right , Alt+0147 left
+
+/*
+let i = 1;
+while (i <10) {
+    console.log("Ahoj");
+    i += 1;              //  ( i++; i = i + 1; i += 1 )
+}
+*/
+
+/* task 1
+let user = Number(prompt("How many times would you like to show „#“?"));
+
+if (user == 0) {
+    console.log("Nothing to show!!");
+} else { 
+    user >= 1;
+    while(user = 100) {
+        console.log(`# ${user}x`);
+        user++;
+    }
+}
 
 
-
-
+*/
 
 
